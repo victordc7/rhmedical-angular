fd62877fe3fff9fa2cd3ef04619e533e204a7f9e
VictorDC7 <victordc704@icloud.com>
2019-10-14 22:14:37 -0400

    navbar, seccion 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  {
+    path: 'inicio',
+    component: DashboardComponent
+  },
+  {
+    path: 'productos',
+    component: ProductsComponent
+  },
+  { path: '**', redirectTo: 'inicio' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './material/material.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Section1Component } from './dashboard/section1/section1.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Section2Component } from './dashboard/section2/section2.component';
+import { Section3Component } from './dashboard/section3/section3.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Section1Component,
+    DashboardComponent,
+    Section2Component,
+    Section3Component,
+    ProductsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HttpClient
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
